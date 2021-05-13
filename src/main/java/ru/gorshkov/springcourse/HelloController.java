926f839feec60b273f76916fd6162ed887652067
Gorshkov Sergey <Gsn82@mail.ru>
2021-05-13 15:21:01 +0500

    16

--- src/main/java/ru/gorshkov/springcourse/HelloController.java
+++ src/main/java/ru/gorshkov/springcourse/HelloController.java
@@ -6,9 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HelloController {
-
     @GetMapping("/hello-world")
-    public String sayHello(){
+    public String sayHello() {
         return "hello_world";
     }
 }
